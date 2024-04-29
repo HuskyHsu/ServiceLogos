7e4d2ae7980ca0150d9afb250fd950a429e11dd7
HuskyHsu <schi.hsu@gmail.com>
2024-04-29 22:05:31 +0800

    fixed filter feature

--- page/app.js
+++ page/app.js
@@ -23,103 +23,112 @@ const logos = data.flatMap((row) => {
   });
 });
 
-const contentDiv = document.querySelector(".content");
-const downloadButton = document.querySelector(".download button");
-const searchInput = document.querySelector(".download button");
-
-function updateList(keyword) {
-  contentDiv.innerHTML = "";
-  const fragment = document.createDocumentFragment();
-  downloadButton.style.display = "none";
-
-  logos
-    .filter((item) => item.title.toLowerCase().includes(keyword.toLowerCase()))
-    .forEach((item) => {
-      const card = document.createElement("div");
-      card.className = "card";
-
-      const checkDiv = document.createElement("div");
-      checkDiv.className = "check";
-
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.id = item.id;
-      checkbox.name = item.id;
-      checkbox.checked = item.checked;
-      if (checkbox.checked) {
-        card.classList.toggle("checked");
-      }
-      checkbox.addEventListener("change", (e) => {
-        card.classList.toggle("checked");
-        item.checked = e.target.checked;
-
-        downloadButton.style.display = logos.some((logo) => logo.checked)
-          ? "flex"
-          : "none";
-      });
-
-      const label = document.createElement("label");
-      label.htmlFor = item.id;
-
-      const checkboxDiv = document.createElement("div");
-      checkboxDiv.className = "checkbox";
-
-      const checkImg = document.createElement("img");
-      checkImg.src = "./asset/check.svg";
-      checkImg.alt = "check";
-
-      checkboxDiv.appendChild(checkImg);
-      label.appendChild(checkboxDiv);
-
-      const title = document.createElement("div");
-      title.className = "title";
-      title.appendChild(document.createTextNode(item.title));
-
-      if (Boolean(item.subTitle)) {
-        const subTitleSpan = document.createElement("span");
-        subTitleSpan.textContent = `(${item.subTitle})`;
-        if (item.subTitle.length > 10) {
-          title.className = "titleCol";
-        }
-        title.appendChild(subTitleSpan);
-      }
-      label.appendChild(title);
-
-      const a = document.createElement("a");
-      a.className = "downloadLink";
-      a.href = `../${item.url}`;
-      a.download = item.url.split("/")[1];
-      const downloadImg = document.createElement("img");
-      downloadImg.src = "./asset/download_.svg";
-      downloadImg.alt = "download";
-      a.appendChild(downloadImg);
-
-      checkDiv.appendChild(checkbox);
-      checkDiv.appendChild(label);
-      checkDiv.appendChild(a);
-
-      const logoImg = document.createElement("img");
-      logoImg.className = "logo";
-      logoImg.src = `../${item.url}?raw=true`;
-      logoImg.alt = item.id;
-
-      card.appendChild(checkDiv);
-      card.appendChild(logoImg);
-
-      fragment.appendChild(card);
-    });
-
-  contentDiv.appendChild(fragment);
+const targetDownloadButton = () => {
   downloadButton.style.display = logos.some((logo) => logo.checked)
     ? "flex"
     : "none";
+};
+
+const contentDiv = document.querySelector(".content");
+const downloadButton = document.querySelector(".download button");
+const searchInput = document.querySelector("#search");
+
+function genList() {
+  contentDiv.innerHTML = "";
+  downloadButton.style.display = "none";
+
+  const cards = logos.map((item) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const checkDiv = document.createElement("div");
+    checkDiv.className = "check";
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = item.id;
+    checkbox.name = item.id;
+    checkbox.checked = item.checked;
+    if (checkbox.checked) {
+      card.classList.toggle("checked");
+    }
+    checkbox.addEventListener("change", (e) => {
+      card.classList.toggle("checked");
+      item.checked = e.target.checked;
+
+      targetDownloadButton();
+    });
+
+    const label = document.createElement("label");
+    label.htmlFor = item.id;
+
+    const checkboxDiv = document.createElement("div");
+    checkboxDiv.className = "checkbox";
+
+    const checkImg = document.createElement("img");
+    checkImg.src = "./asset/check.svg";
+    checkImg.alt = "check";
+
+    checkboxDiv.appendChild(checkImg);
+    label.appendChild(checkboxDiv);
+
+    const title = document.createElement("div");
+    title.className = "title";
+    title.appendChild(document.createTextNode(item.title));
+
+    if (Boolean(item.subTitle)) {
+      const subTitleSpan = document.createElement("span");
+      subTitleSpan.textContent = `(${item.subTitle})`;
+      if (item.subTitle.length > 10) {
+        title.className = "titleCol";
+      }
+      title.appendChild(subTitleSpan);
+    }
+    label.appendChild(title);
+
+    const a = document.createElement("a");
+    a.className = "downloadLink";
+    a.href = `../${item.url}`;
+    a.download = decodeURIComponent(item.url).split("/")[1];
+    const downloadImg = document.createElement("img");
+    downloadImg.src = "./asset/download_.svg";
+    downloadImg.alt = "download";
+    a.appendChild(downloadImg);
+
+    checkDiv.appendChild(checkbox);
+    checkDiv.appendChild(label);
+    checkDiv.appendChild(a);
+
+    const logoImg = document.createElement("img");
+    logoImg.className = "logo";
+    logoImg.src = `../${item.url}?raw=true`;
+    logoImg.alt = item.id;
+
+    card.appendChild(checkDiv);
+    card.appendChild(logoImg);
+
+    return card;
+  });
+
+  contentDiv.append(...cards);
+  targetDownloadButton();
+
+  return cards;
 }
 
-updateList("");
+const cards = genList();
 searchInput.addEventListener("input", function () {
-  updateList(this.value);
+  const keyword = this.value;
+  cards.forEach((card, i) => {
+    const item = logos[i];
+    if (item.title.toLowerCase().includes(keyword.toLowerCase())) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
 });
 
-searchInput.addEventListener("click", function () {
-  console.log("xd");
+downloadButton.addEventListener("click", function () {
+  console.log("qq");
 });
